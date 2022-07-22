6c8d3a2bf716fecb1afd1b0a2c7814167ae1cb21
kanchanbarwade <kanchan.barwade@gmail.com>
2022-07-22 15:57:58 +0530

    Removed commented lines

--- src/pages/Test1.java
+++ src/pages/Test1.java
@@ -12,30 +12,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Test1
- */
+
 @WebServlet({ "/test1" })
 public class Test1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
+	
 	public void init() throws ServletException {
 		System.out.println("in init of  "+getClass()+"invoked by "+Thread.currentThread());
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
+	
 	public void destroy() {
 		System.out.println("in destroy of  "+getClass()+"invoked by "+Thread.currentThread());
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("in  of doGet "+getClass()+"invoked by "+Thread.currentThread());
 		response.setContentType("text/html");
